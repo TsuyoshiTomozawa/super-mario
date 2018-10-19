3fd4230139c3a92062679e9bf5ad54029bedc4f5
TsuyoshiTomozawa <sphdm244@gmail.com>
2018-10-19 18:26:08 +0900

    マリオ表示

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,45 @@
-import SpriteSheet from './SpriteSheet.js';
-import {loadImage, loadLevel} from './loaders.js';
+import Compositor from './Compositor.js';
+import {loadLevel} from './loaders.js';
+import {loadMarioSprites, loadBackgroundSprites} from './sprite.js';
+import {createBackgroungLayer} from './layer.js';
 
-
-function drawBackground(background, context, sprites) {
-	background.ranges.forEach(([x1, x2, y1, y2]) => {
-		for (let x =x1; x < x2; ++x) {
-			for (let y = y1; y < y2; ++y) {
-				sprites.drawTiles(background.tile, context, x, y);		
-			}
-		}
-	});
-}
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-
-loadImage('/img/tiles.png')
-.then(image => {
-	const sprites = new SpriteSheet(image, 16, 16);
-	sprites.define('ground', 0, 0);
-	sprites.define('sky', 3, 23);
-	
-
-	loadLevel('1-1')
-	.then(level => {
-		level.background.forEach(background => {
-			drawBackground(background, context, sprites);
-		});
+function createSpriteLayer(sprite, pos) {
+	return function drawSpriteLayer(context) {
+		for (let i = 0; i < 20; ++i) {
+			sprite.draw('idle', context, pos.x + i * 16, pos.y);	
+		}
 		
-	});
+	}
+}
 
+//Promise オブジェ loadBackgroundSprites, loadLevelがresolveされたらthenを呼ぶ
+Promise.all([
+	loadMarioSprites(),
+	loadBackgroundSprites(),
+	loadLevel('1-1'),
+])
+.then(([marioSprites, backgroundSprites,level]) => {
+	const comp = new Compositor();
+	const backgroundLayer = createBackgroungLayer(level.backgrounds, backgroundSprites);		
+	comp.layers.push(backgroundLayer);
+
+	const pos = {
+		x: 0,
+		y: 0,
+	}
+
+	const spriteLayer = createSpriteLayer(marioSprites, pos);
+	comp.layers.push(spriteLayer);
+
+	function update() {
+		comp.draw(context);
+		pos.x += 2;
+		pos.y += 2;
+		requestAnimationFrame(update);	
+	}
+
+	update();
 });
